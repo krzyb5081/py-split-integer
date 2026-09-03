0e32a85dddfaeea8f4988d49a932f8efe35170e2
Krzysztof Sobczak <krzysztof.sobczak5081@gmail.com>
2026-09-03 15:58:31 +0200

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,9 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
+    assert (
+        sum(split_integer(8, 2)) == 8
+    ), "Sum of the parts should be equal to value"
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
